ee3eadbbcfef590de9998fec66c19b619ddf6c16
Spencer Davis <spencerdavis2000@yahoo.com>
2016-07-08 01:57:06 +0000

    added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,6 +8,11 @@ class ContactsController < ApplicationController
       
       #this gets saved to the database
       if @contact.save 
+         name = params[:contact][:name]
+         email = params[:contact][:email]
+         body = params[:contact][:comments]
+          
+         ContactMailer.contact_email(name, email, body).deliver
          flash[:success] = "Message sent."
          redirect_to new_contact_path
       else
---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,15 @@
+# we had to write this by hand not premade
+
+class ContactMailer < ActionMailer::Base
+  default to: 'spencerdavis2000@gmail.com'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Contact Form Message')
+  end
+    
+    
+end
