3c205054a4b0f9a8570f1ad3fb038a9d4ecad900
Spencer Davis <spencerdavis2000@yahoo.com>
2016-07-24 14:23:36 +0000

    took out 'host' in environment.rb because it might be messing with sending emails and it was not needed in directions

--- config/environment.rb
+++ config/environment.rb
@@ -12,6 +12,5 @@ ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
-  :host => 'sleepy-sands-4781.herokuapp.com',
   :enable_startstls_auto => true
 }
